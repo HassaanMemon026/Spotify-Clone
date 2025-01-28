4c1aa3f0558a3846d75685ce2189a68c52cb76f7
Muhammad Hassaan <162817769+HassaanMemon026@users.noreply.github.com>
2025-01-28 07:00:41 +0500

    Update script.js
    FIxed some bugs

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ let songs;
 let currenFolder;
 let currentFolderName;
 let mainFolder = "mp3/"
-let localfatch = "";
 let index;
 
 let previous = document.querySelector("#previous");
 let play = document.querySelector("#play");
 let next = document.querySelector("#next")
 async function getFolder(folder) {
-    let a = await fetch(`${localfatch}${folder}/`);
+    let a = await fetch(`${folder}/`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -19,7 +18,7 @@ async function getFolder(folder) {
     Array.from(as).forEach(async (e) => {
         if (e.href.includes("mp3/") && e.href.endsWith("/")) {
             let folder = e.innerText.slice(0, -1)
-            let a = await fetch(`${localfatch}mp3/${folder}/info.json`);
+            let a = await fetch(`mp3/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + `<div class="card" data-folder="${folder}">
                         <div class="play">
@@ -51,7 +50,7 @@ function secondsToMinutesSeconds(seconds) {
 }
 async function getSong(folder) {
     currenFolder = folder;
-    let a = await fetch(`${localfatch}${folder}/`);
+    let a = await fetch(`${folder}/`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -69,7 +68,7 @@ async function getSong(folder) {
 
 async function getSongName(folder) {
     currenFolder = folder;
-    let a = await fetch(`${localfatch}${folder}/`);
+    let a = await fetch(`${folder}/`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
